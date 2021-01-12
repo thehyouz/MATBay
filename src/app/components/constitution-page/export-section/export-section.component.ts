98e5c95d0c9c7d0b5a105aac467f1fea9b42f82e
H.V <hugo.vdbm@icloud.com>
2021-01-12 12:11:26 -0500

    nouveau format export

--- src/app/components/constitution-page/export-section/export-section.component.ts
+++ src/app/components/constitution-page/export-section/export-section.component.ts
@@ -6,6 +6,9 @@ import { Constitution } from 'src/app/types/constitution';
 import { Song } from 'src/app/types/song';
 import { User } from 'src/app/types/user';
 
+const BASIC_TXT_HEADER: string = " ID |        Titre       |       Auteur       |     Ajouté Par      \n";
+const TXT_SEPARATOR: string = "================================================================ \n"
+
 @Component({
   selector: 'app-export-section',
   templateUrl: './export-section.component.html',
@@ -24,24 +27,66 @@ export class ExportSectionComponent {
   constructor() { }
 
   convertSongArrayToString(songs: Song[]): string {
-    let text = "";
+    let text = BASIC_TXT_HEADER + TXT_SEPARATOR;
     for (const song of songs) {
-      text += song.id + "   " + song.author + "   " + song.shortTitle + "\n";
+      text += this.correctStringLength(song.id.toString(), 5) + "|" + this.correctStringLength(song.shortTitle, 26) + "|" + this.correctStringLength(song.author, 26) + "|" + this.correctStringLength(this.showDisplayName(song.patron), 25) + "\n";
     }
     return text;
+  }
+
+  convertSongArrayToGoogleSheets(songs: Song[]): string {
+    let text = "";
+    for (const song of songs) {
+      text += '=LIEN_HYPERTEXTE("' + song.url + '"; "' + song.shortTitle + '")' + "\n";
+    }
+    return text;
+  }
+
+  correctStringLength(string: string, length: number): string {
+    console.log("string l:", string.length);
+    console.log("l", length);
+    console.log("d", length - string.length);
+
+    if (string.length < length) {
+      for (let i = 0; i < length - string.length + 1; i++) {
+        string += " ";
+      }
+    }
+    return string;
   }
 
   returnConstitutionData() {
     return of(this.constitution.songs);
   }
 
-  dynamicDownloadTxt() {
+  dynamicDownloadTxt(format: string) {
     this.returnConstitutionData().subscribe((songs) => {
-      this.dyanmicDownloadByHtmlTag({
-        fileName: this.constitution.name,
-        // text: JSON.stringify(songs, null, 2)
-        text: this.convertSongArrayToString(songs)
-      });
+      switch (format) {
+        case "song-list":
+          this.dyanmicDownloadByHtmlTag({
+            fileName: this.constitution.name,
+            text: this.convertSongArrayToString(songs)
+          });
+          break;
+        case "json":
+          this.dyanmicDownloadByHtmlTag({
+            fileName: this.constitution.name,
+            text: JSON.stringify(songs, null, 2)
+          });
+          break;
+        case "sheets":
+          this.dyanmicDownloadByHtmlTag({
+            fileName: this.constitution.name,
+            text: this.convertSongArrayToGoogleSheets(songs)
+          });
+          break;
+        default:
+          this.dyanmicDownloadByHtmlTag({
+          fileName: this.constitution.name,
+          text: this.convertSongArrayToString(songs)
+        });
+      }
+      
     });
   }
 
@@ -58,5 +103,10 @@ export class ExportSectionComponent {
     element.dispatchEvent(event);
   }
 
+  showDisplayName(uid: string): string {
+    const user = this.users.find(x => x.uid === uid);
+    if (user !== undefined) { return user.displayName; }
+    return "";
+  }
 
 }
